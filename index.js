d6fb0eb555347bd6c66abb154c5f55f810cac98b
Neel <neel.gorasiya@fxis.ai>
2025-01-16 15:22:38 +0530

    added schema and resolvers with sample data

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+//Database
+import db from "./_db.js";
+
+// Types
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+        reviews: () => db.reviews,
+        review: (_, args, context, info) => {
+            return db.reviews.find(review => review.id === args.id);
+        },
+        games: () => db.games,
+        game: (_, args, context, info) => {
+            return db.games.find(game => game.id === args.id);
+        },
+        authors: () => db.authors,
+        author: (_, args, context, info) => {
+            return db.authors.find(author => author.id === args.id);
+        },
+    },
+}
+
 //Server setup
 const server = new ApolloServer({
-    // typeDefs and resolvers are required
+    // typeDefs -- definitions of types of data
+    typeDefs,
+    // Resolvers -- functions that return data for each type
+    resolvers
+
     
 })
 
